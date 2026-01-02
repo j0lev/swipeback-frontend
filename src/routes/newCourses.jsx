6f67ce42955dd436f2304d9f601c0daf41c7d71f
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-03 00:13:05 +0100

    syling vas structure with bootstrap

--- src/routes/newCourses.jsx
+++ src/routes/newCourses.jsx
@@ -1,77 +1,212 @@
-function NewCourse(){
+function NewCourse() {
 
 
-    return(
+    return (
         <>
             <h3>Add New Course</h3>
-            <div>
-                <form action="/hiermus die anfrageseite stehen" method="post">
-                    <label htmlFor="cname">Cours Name</label>
-                    <input type="text" id="cname" name="cname" placeholder="Enter Cours Name"/>
-                    <div>
-                        <label htmlFor="stdate">Start Date</label>
-                        <input type="date" id="stdate" name="stdate"/>
-                        <label htmlFor="enddate">End Date</label>
-                        <input type="date" id="enddate" name="enddate"/>
+            <div className="card">
 
-                    </div>
-                    <div>
-                        <h4>Choose Weekdays</h4>
-                        <input type="checkbox" id="wdmon" name="wdmon" value={"monday"} selected/>
-                        <label htmlFor="wdmon">Monady</label>
-                        <input type="checkbox" id="wdtue" name="wdtue" value={"tuesday"} />
-                        <label htmlFor="wdtue">Tuesday</label>
-                        <input type="checkbox" id="wdwnd" name="wdwnd" value={"wendsday"} />
-                        <label htmlFor="wdwnd">Wendsday</label>
-                        <input type="checkbox" id="wdthr" name="wdthr" value={"thirsday"} />
-                        <label htmlFor="wdthr">Thirsday</label>
-                        <input type="checkbox" id="wdfri" name="wdfri" value={"friday"} />
-                        <label htmlFor="wdfri">Friday</label>
-                        <input type="checkbox" id="wdsat" name="wdsat" value={"saturday"} />
-                        <label htmlFor="wdsat">Saturday</label>
-                        <input type="checkbox" id="wdsun" name="wdsun" value={"sunday"} />
-                        <label htmlFor="wdsun">Sunday</label>
-                        <div>
-                            <label htmlFor="sttime">Satrt time</label>
-                            <input type="time" id="sttime" name="sttime"/>
-                            <label htmlFor="endtime">End time</label>
-                            <input type="time" id="endtime" name="endtime"/>
+                <div className="card-body">
+
+                    <form action="/hiermus die anfrageseite stehen" method="post">
+
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-3">
+                                    <h4 >Cours Name</h4>
+                                </div>
+                                <div className="col-9">
+                                    <input type="text" id="cname" name="cname" className="form-control" placeholder="Enter Cours Name" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-3">
+                                    <h4>Term</h4>
+                                </div>
+                                <div className="col-9">
+                                    <div className="card p-0">
+                                        <div className="card-body">
+                                            <div className="row">
+                                                <div className="col-2">
+                                                    <label htmlFor="stdate" className="col-form-label">Start Date</label>
+                                                </div>
+                                                <div className="col-3">
+                                                    <input type="date" id="stdate" name="stdate" className="form-control" />
+                                                </div>
+                                                <div className="col-2">
+
+                                                </div>
+                                                <div className="col-2">
+                                                    <label htmlFor="enddate" className="col-form-label">End Date</label>
+                                                </div>
+                                                <div className="col-3">
+                                                    <input type="date" id="enddate" name="enddate" className="form-control" />
+                                                </div>
+                                            </div>
+
+
+
+
+
+                                        </div>
+
+                                    </div>
+                                    <div className="card p-0">
+                                        <div className="card-body">
+                                            <div className="container">
+                                                <div className="row">
+                                                    <div className="input-group d-flex w-100 justify-content-center">
+                                                        <label htmlFor="wdmon" className="btn btn-outline-primary">MON</label>
+                                                        <input type="checkbox" className="btn-check" id="wdmon" name="wdmon" value={"monday"} selected />
+                                                        <input type="checkbox" className="btn-check" id="wdtue" name="wdtue" value={"tuesday"} />
+                                                        <label htmlFor="wdtue" className="btn btn-outline-primary">TUE</label>
+                                                        <input type="checkbox" className="btn-check" id="wdwnd" name="wdwnd" value={"wendsday"} />
+                                                        <label htmlFor="wdwnd" className="btn btn-outline-primary">WEN</label>
+                                                        <input type="checkbox" className="btn-check" id="wdthr" name="wdthr" value={"thirsday"} />
+                                                        <label htmlFor="wdthr" className="btn btn-outline-primary">THI</label>
+                                                        <input type="checkbox" className="btn-check" id="wdfri" name="wdfri" value={"friday"} />
+                                                        <label htmlFor="wdfri" className="btn btn-outline-primary">FRI</label>
+                                                        <input type="checkbox" className="btn-check" id="wdsat" name="wdsat" value={"saturday"} />
+                                                        <label htmlFor="wdsat" className="btn btn-outline-primary">SAT</label>
+                                                        <input type="checkbox" className="btn-check" id="wdsun" name="wdsun" value={"sunday"} />
+                                                        <label htmlFor="wdsun" className="btn btn-outline-primary">SUN</label>
+                                                    </div>
+                                                </div>
+                                                <div className="row">
+                                                    <div className="col-2">
+                                                        <h5> Monday</h5>
+                                                    </div>
+                                                    <div className="col-2">
+                                                        <label htmlFor="sttime" className="col-form-label">Start time</label>
+                                                    </div>
+                                                    <div className="col-3">
+                                                        <input type="time" id="sttime" name="sttime" className="form-control" />
+                                                    </div>
+
+                                                    <div className="col-2">
+                                                        <label htmlFor="endtime" className="col-form-label">End time</label>
+                                                    </div>
+                                                    <div className="col-3">
+                                                        <input type="time" id="endtime" name="endtime" className="form-control" />
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-3"><h4>Frequence</h4></div>
+                                <div className="col-9">
+                                    <div className="card p-1">
+                                        <div className="card-body">
+                                            <div className="input-group d-flex w-100 justify-content-center">
+                                                <label htmlFor="Onetime" className="btn btn-outline-primary">One Time</label>
+                                                <input type="radio" className="btn-check" id="Onetime" name="Onetime" value={"Onetime"} />
+
+                                                <input type="radio" className="btn-check" id="wkly" name="wkly" value={"wkly"} selected />
+                                                <label htmlFor="wkly" className="btn btn-outline-primary">Weekly</label>
+
+                                                <input type="radio" className="btn-check" id="bwklyodd" name="bwklyodd" value={"bwklyodd"} />
+                                                <label htmlFor="bwklyodd" className="btn btn-outline-primary">biweekly (ood weeks)</label>
+                                                <input type="radio" className="btn-check" id="bwklyeven" name="bwklyeven" value={"bwklyeven"} />
+                                                <label htmlFor="bwklyeven" className="btn btn-outline-primary">biweekly (even Weeks)</label>
+                                                <input type="radio" className="btn-check" id="mnthly" name="mnthly" value={"mnthly"} />
+                                                <label htmlFor="mnthly" className="btn btn-outline-primary">monthly</label>
+                                            </div></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-3"><h4>Feedback slider</h4></div>
+                                <div className="col-9">
+                                    <div className="card p-0">
+                                        <div className="card-body">
+                                            <div className="container">
+                                                <div className="row">
+                                                    <div className="col-9">
+                                                        <div className="row">
+                                                            <div className="col-2">
+                                                                <label htmlFor="slidername" className="col-form-label">Slidername</label>
+                                                            </div>
+                                                            <div className="col-5">
+                                                                <input type="text" className="form-control" id="slidername" name="slidername" value={"slidername"} />
+                                                            </div>
+                                                            <div className="col-1">
+                                                                <label htmlFor="symbol" className="col-form-label">Logo</label>
+                                                            </div>
+                                                            <div className="col-2" >
+                                                                <select name="symbol" className="form-control" id="symbol">
+                                                                    <option value={""}>Symbol</option>
+                                                                    <option value={"bulb"}>bulb</option>
+                                                                    <option value={"book"}>book</option>
+                                                                    <option value={"lins"}>linse</option>
+                                                                </select>
+                                                            </div>
+                                                            <div className="col-1">
+                                                                <label htmlFor="color" className="col-form-label">Color</label>
+                                                            </div>
+                                                            <div className="col-1">
+                                                                <input type="color" className="form-control form-control-color" id="color" name="color" value={"color"} />
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <div className="col-3">
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="row">
+                                <div className="col-3"><h4>after lecutre swipe questions</h4></div>
+                                <div className="col-9">
+                                    <div className="card p-0">
+                                        <div className="card-body">
+                                            <div className="container">
+                                                <div className="row">
+                                                    <div className="col-9">
+                                                        <div className="row">
+                                                            <div className="col-3">
+                                                                <label htmlFor="slidername" className="col-form-label">Question Title</label>
+                                                            </div>
+                                                            <div className="col-6">
+                                                                <input type="text" className="form-control" id="slidername" name="slidername" value={"slidername"} />
+                                                            </div>
+                                                            <div className="col-1">
+                                                                <label htmlFor="symbol" className="col-form-label">Logo</label>
+                                                            </div>
+                                                            <div className="col-2" >
+                                                                <select name="symbol" className="form-control" id="symbol">
+                                                                    <option value={""}>Symbol</option>
+                                                                    <option value={"bulb"}>bulb</option>
+                                                                    <option value={"book"}>book</option>
+                                                                    <option value={"lins"}>linse</option>
+                                                                </select>
+                                                            </div>
+                                                        </div>
+
+                                                    </div>
+                                                    <div className="col-3">
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <h4>Frequence</h4>
-                        <input type="checkbox" id="Onetime" name="Onetime" value={"Onetime"} />
-                        <label htmlFor="Onetime">One Time</label>
-                        <input type="checkbox" id="wkly" name="wkly" value={"wkly"} selected/>
-                        <label htmlFor="wkly">Weekly</label>
-                        
-                        <input type="checkbox" id="bwklyodd" name="bwklyodd" value={"bwklyodd"} />
-                        <label htmlFor="bwklyodd">biweekly (ood weeks)</label>
-                        <input type="checkbox" id="bwklyeven" name="bwklyeven" value={"bwklyeven"} />
-                        <label htmlFor="bwklyeven">biweekly (even Weeks)</label>
-                        <input type="checkbox" id="mnthly" name="mnthly" value={"mnthly"} />
-                        <label htmlFor="mnthly">monthly</label>
-                        
-
-                    </div>
-
-                    <div>
-                        <h4>Feedback slider</h4>
-                        <label htmlFor="slidername">slidername</label>
-                        <input type="text" id="slidername" name="slidername" value={"slidername"} />
-                        <select name="symbol" id="symbol">
-                            <option value={""}>Select symbol</option>
-                            <option value={"bulb"}>bulb</option>
-                            <option value={"book"}>book</option>
-                            <option value={"lins"}>linse</option>
-                        </select>
-                        <input type="color" id="color" name="color" value={"color"}/>
-
-                    </div>
-                   
-                </form>
+                    </form>
+                </div>
             </div>
-            
         </>
 
     );
