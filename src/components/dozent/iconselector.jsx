94d8cd37dc75a45356f72da14c14da57de62b142
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-09 13:49:08 +0100

    updating design elements

--- src/components/dozent/iconselector.jsx
+++ src/components/dozent/iconselector.jsx
@@ -3,22 +3,22 @@ import { useId } from "react";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 //import { Alarm, Book, Lightbulb } from "react-bootstrap-icons";
 
-function Iconselector(props){
+function Iconselector(props) {
     let iconList = [
         <i class="bi bi-lightbulb"></i>,
         <i class="bi bi-book"></i>,
         <i class="bi bi-alarm"></i>
     ]
     let defaulttext = "Logo";
-    if(props.selected !=null){
-        defaulttext = iconList [props.selected];
+    if (props.selected != null) {
+        defaulttext = iconList[props.selected];
     }
     let buttonid = useId();
-    let onClickDropdown = (evt) =>{
+    let onClickDropdown = (evt) => {
         let target = evt.target;
         console.log(target.tagName);
-        while(target.tagName!="A"){
-            target=target.parentNode;
+        while (target.tagName != "A") {
+            target = target.parentNode;
         }
         console.log(target)
         //evt.preventDefault();
@@ -27,17 +27,17 @@ function Iconselector(props){
 
     return (
         <>
-        <div class="btn-group">
-  <button type="button" class="btn dropdown-toggle"  id={buttonid} data-bs-toggle="dropdown" aria-expanded="false">
-    {defaulttext}
-  </button>
-  <ul class="dropdown-menu">
-    {iconList.map(icon => {
-        return (<li><a class="dropdown-item" onClick={onClickDropdown} href="#">{icon}</a></li>)
-    })}
-  </ul>
-</div>
-        
+            <div class="btn-group">
+                <button type="button" class="btn border dropdown-toggle" id={buttonid} data-bs-toggle="dropdown" aria-expanded="false">
+                    {defaulttext}
+                </button>
+                <ul class="dropdown-menu">
+                    {iconList.map(icon => {
+                        return (<li><a class="dropdown-item" onClick={onClickDropdown}>{icon}</a></li>)
+                    })}
+                </ul>
+            </div>
+
         </>
     );
 }
--- src/components/dozent/settingSwipeQuestion.jsx
+++ src/components/dozent/settingSwipeQuestion.jsx
@@ -12,13 +12,13 @@ function SettingSwipeQuestion(props) {
                                 <div className="col-3">
                                     <label htmlFor={props.id+"questionname"} className="col-form-label">Question Title</label>
                                 </div>
-                                <div className="col-6">
+                                <div className="col-5">
                                     <input type="text" className="form-control" id={props.id+"questionname"} name={props.id+"questionname"}/>
                                 </div>
                                 <div className="col-1">
                                     <label htmlFor={props.id+"qsymbol"} className="col-form-label">Logo</label>
                                 </div>
-                                <div className="col-2" >
+                                <div className="col-3" >
                                     <Iconselector id={props.id+"qsymbol"}></Iconselector>
                                     
                                 </div>
