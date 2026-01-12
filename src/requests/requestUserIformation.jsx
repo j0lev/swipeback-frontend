6099c59690e6da8be068d94b1ffdbcc53cdd4059
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-12 19:46:13 +0100

    updating the request url

--- src/requests/requestUserIformation.jsx
+++ src/requests/requestUserIformation.jsx
@@ -1,6 +1,6 @@
 function RequestUserInformation(user,setUser){
     var http = new XMLHttpRequest();
-    const link = "https://swipeback-backend.onrender.com/user/me";
+    const link = "https://swipeback-backend.onrender.com/user/me/";
 
     http.open('GET', link, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
