ac3179a9921879311137c9cd19be6e787880f0bf
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-12 19:48:50 +0100

    secont update to reqesturl

--- src/requests/requestUserIformation.jsx
+++ src/requests/requestUserIformation.jsx
@@ -1,10 +1,10 @@
 function RequestUserInformation(user,setUser){
     var http = new XMLHttpRequest();
-    const link = "https://swipeback-backend.onrender.com/user/me/";
+    const link = "https://swipeback-backend.onrender.com/users/me/";
 
     http.open('GET', link, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    http.setRequestHeader('Authorization', 'Bearer ' + user.access_token);
+    http.setRequestHeader('Authorization', 'bearer ' + user.access_token);
     http.onload = function () {
         console.log(this.responseText);
     };
