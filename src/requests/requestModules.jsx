06631954e61f1a7d97750c0702c3ba35f108daba
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-18 17:06:41 +0100

    adding mor backend connections

--- src/requests/requestModules.jsx
+++ src/requests/requestModules.jsx
@@ -1,21 +1,25 @@
-function RequestUserInformation(user, setUser) {
+
+
+function RequestModuleList(setData, user) {
     var http = new XMLHttpRequest();
-    const link = "https://swipeback-backend.onrender.com/users/me/";
+    const link = "https://swipeback-backend.onrender.com/modules/";
 
     http.open('GET', link, true);
     http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     http.setRequestHeader('Authorization', 'bearer ' + user.access_token);
     http.onload = function () {
         let result = JSON.parse(this.responseText);
-        if (result.username != null) {
-            setUser({
-                ...user,
-                ...result
+        if (result.title != null) {
+            setData({
+                notLoaded: false,
+                moduleList: result
             })
         }
     };
 
-    http.send();
-}
+        http.send();
+    }
 
-export default RequestUserInformation
+export{
+    RequestModuleList
+}
--- src/routes/dozent/dozentpage.jsx
+++ src/routes/dozent/dozentpage.jsx
@@ -1,95 +1,109 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "../../context/authenticationContext";
 import RequestUserInformation from "../../requests/requestUserIformation";
+import { RequestModuleList } from "../../requests/requestModules";
 
 
 function Dozentpage() {
-    let {user, setUser} = useContext(AuthenticationContext);
+    let { user, setUser } = useContext(AuthenticationContext);
     let navigate = useNavigate();
-    if(user.access_token == null){
+    let [ modules, setModules ] = useState({ notLoaded: true });
+    if (user.access_token == null) {
         navigate("/");
-    }else{
-        if(user.name==null){
+        setUser({ ...user });
+    } else {
+        if (user.name == null) {
             RequestUserInformation(user, setUser);
+            RequestModuleList(setModules, user);
         }
     }
-    let datatocourses = [{
-        name: "Linalg",
-        nextdate: new Date(),
-        id:124243
-    }]
 
-    
 
-    let onClickLogout =()=>{
-        setUser({});
+
+
+    let onClickLogout = () => {
+        setUser({ access_token: null });
         //hier muss die session beendet werden
-        navigate("/");
+        //navigate("/");
     }
-
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 justify-content-center">
-                        <h3>Welcome Marcel D'Avis</h3>
-
-                    </div>
-                </div>
-                <div className="row pb-3" >
-                    <div className="col-3">
-                        logoplatz
-                    </div>
-                    <div className="col-9 d-flex justify-content-start">
-                        <a className="btn btn-secondary" href="doz/newCourse">add new Course</a>
-                    </div>
-                    <div className="col-2">
-                        <a className="btn btn-secondary" onClick={onClickLogout}>Logout</a>
-                    </div>
-
-                </div>
-
-                <div className="row">
-                    <div className="card">
-                        <div className="card-header">My Courses </div>
-                        <div className="card-body">
-                            {datatocourses.map(data => {
-                                return (
-                                    <>
-                                        <div className="row">
-                                            <div className="card p-3">
-                                                <div className="card-body">
-                                                    <div className="row" >
-                                                        <div className="col-8 d-flex justify-content-start" >
-                                                            Course: {data.name}
-                                                        </div>
-                                                        <div className="col-4 d-flex justify-content-end">
-                                                            next lecture: {data.nextdate.toLocaleDateString()} {data.nextdate.toLocaleTimeString()}
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="row">
-                                                    <div className="col-3 d-flex justify-content-start">
-                                                        <button className="btn btn-secondary">View Dashboard</button>
-                                                    </div>
-                                                    <div className="col-3 d-flex justify-content-start">
-                                                        <a className="btn btn-secondary" href={"doz/editCourse/"+data.id}>edit</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </>
-                                )
-                            })}
+    console.log(modules);
+    if (user.full_name != null) {
+        return (
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 justify-content-center">
+                            <h3>Welcome {user.full_name}</h3>
 
                         </div>
                     </div>
-                </div>
-            </div>
-        </>
+                    <div className="row pb-3" >
+                        <div className="col-3">
+                            logoplatz
+                        </div>
+                        <div className="col-9 d-flex justify-content-start">
+                            <a className="btn btn-secondary" href="doz/newCourse">add new Course</a>
+                        </div>
+                        <div className="col-2">
+                            <a className="btn btn-secondary" onClick={onClickLogout}>Logout</a>
+                        </div>
 
-    );
+                    </div>
+
+                    <div className="row">
+                        <div className="card">
+                            <div className="card-header">My Courses </div>
+                            <div className="card-body">
+                                {
+
+
+                                    (modules.notLoaded ?
+                                        <div class="d-flex justify-content-center">
+                                            <div class="spinner-border" role="status">
+                                                <span class="sr-only"></span>
+                                            </div>
+                                        </div>
+                                        :
+                                        modules.moduleList.map(data => {
+                                            return (<>
+                                                <div className="row">
+                                                    <div className="card p-3">
+                                                        <div className="card-body">
+                                                            <div className="row" >
+                                                                <div className="col-8 d-flex justify-content-start" >
+                                                                    Course: {data.title}
+                                                                </div>
+                                                                <div className="col-4 d-flex justify-content-end">
+                                                                    Created At: {data.created_at.toLocaleDateString()} {data.created_at.toLocaleTimeString()}
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-3 d-flex justify-content-start">
+                                                                <button className="btn btn-secondary">View Dashboard</button>
+                                                            </div>
+                                                            <div className="col-3 d-flex justify-content-start">
+                                                                <a className="btn btn-secondary" href={"doz/editCourse/" + data.id}>edit</a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </>)
+                                        }))
+
+
+                                }
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+
+        );
+    }
 
 }
 
