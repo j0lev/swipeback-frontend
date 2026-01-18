d7dbdac3bd6d0c2051df659899a7289fb5c94320
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-19 00:20:04 +0100

    chainging response code

--- src/requests/requestModules.jsx
+++ src/requests/requestModules.jsx
@@ -84,7 +84,7 @@ function RequestDeleteModule(id, user, onSuccess) {
     http.setRequestHeader('Content-type', 'application/json');
     http.setRequestHeader('Authorization', 'bearer ' + user.access_token);
     http.onload = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 204) {
             onSuccess();
         }
     };
