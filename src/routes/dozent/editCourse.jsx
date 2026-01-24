af0c953f1c9d8842bea3d942ce1f97953df208de
Nick Eggert <nick.eggert@stud.uni-hannover.de>
2026-01-24 16:42:26 +0100

    bugfix

--- src/routes/dozent/editCourse.jsx
+++ src/routes/dozent/editCourse.jsx
@@ -191,7 +191,9 @@ function NewCourse() {
             }
             RequestUpdateModule(document.getElementById("cname").value, fbnr, user, onHandleData)
             for (let i = 0; i < feedbackslider.length; i++) {
+                if( document.getElementById(feedbackslider[i] + "slidername")!=null){
                     RequestAddSlider(fbnr, user, document.getElementById(feedbackslider[i] + "slidername").value)
+                }
                 }
 
         }
